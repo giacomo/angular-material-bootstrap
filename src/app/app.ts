f921850bdb8a031ed3ceab50438111af17e37cd9
Giacomo Barbalinardo <info@ready24it.eu>
2025-12-15 07:14:50 +0100

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  standalone: false,
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('angular-material-bootstrap2');
+}
